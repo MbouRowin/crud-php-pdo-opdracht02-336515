2b5cfaeb30075bdbc8a8da29f8932ed414fc514a
MbouRowin <117640285+MbouRowin@users.noreply.github.com>
2023-02-03 13:14:49 +0100

    Add create

--- create.php
+++ create.php
@@ -1,4 +1,25 @@
 <?php
+
+require_once "database.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $bodemformaat = $_POST["bodemformaat"] ?? "";
+    $saus = $_POST["saus"] ?? "";
+    $pizzatopping = $_POST["pizzatopping"] ?? "";
+    $kruiden = implode(", ", $_POST["kruiden"] ?? []);
+
+    $stmt = $pdo->prepare("INSERT INTO pizza (bodemformaat, saus, pizzatopping, kruiden) VALUES (?, ?, ?, ?)");
+    $stmt->bindValue(1, $bodemformaat);
+    $stmt->bindValue(2, $saus);
+    $stmt->bindValue(3, $pizzatopping);
+    $stmt->bindValue(4, $kruiden);
+
+    $stmt->execute();
+
+    header("Refresh: 2; url=/");
+    die("De pizza is aangemaakt.");
+}
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -20,42 +41,42 @@
 
                 <select name="bodemformaat" id="bodemformaat" class="form-control">
                     <option>Maak je keuze</option>
-                    <option value="20">20 cm</option>
-                    <option value="25">25 cm</option>
-                    <option value="30">30 cm</option>
-                    <option value="35">35 cm</option>
-                    <option value="40">40 cm</option>
+                    <option value="20 cm">20 cm</option>
+                    <option value="25 cm">25 cm</option>
+                    <option value="30 cm">30 cm</option>
+                    <option value="35 cm">35 cm</option>
+                    <option value="40 cm">40 cm</option>
                 </select>
             </div>
 
             <div class="mb-3">
-                <label for="bodemformaat" class="form-label">Saus</label>
+                <label for="saus" class="form-label">Saus</label>
 
-                <select name="bodemformaat" id="bodemformaat" class="form-control">
+                <select name="saus" id="saus" class="form-control">
                     <option>Maak je keuze</option>
-                    <option value="tomatensaus">Tomatensaus</option>
-                    <option value="extra-tomatensaus">Extra tomatensaus</option>
-                    <option value="spicy-tomatensaus">Spicy tomatensaus</option>
-                    <option value="bbq-saus">BBQ saus</option>
-                    <option value="creme-fraiche">Creme fraiche</option>
+                    <option value="Tomatensaus">Tomatensaus</option>
+                    <option value="Extra tomatensaus">Extra tomatensaus</option>
+                    <option value="Spicy tomatensaus">Spicy tomatensaus</option>
+                    <option value="BBQ saus">BBQ saus</option>
+                    <option value="Creme fraiche">Creme fraiche</option>
                 </select>
             </div>
 
             <div class="mb-3">
-                <div class="form-label">Pizzatoppings</div>
+                <div class="form-label">Pizzatopping</div>
 
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="pizzatoppings" id="vegan">
+                    <input class="form-check-input" type="radio" name="pizzatopping" value="Vegan" id="vegan">
                     <label class="form-check-label" for="vegan"> Vegan</label>
                 </div>
 
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="pizzatoppings" id="vegetarisch">
+                    <input class="form-check-input" type="radio" name="pizzatopping" value="Vegetarisch" id="vegetarisch">
                     <label class="form-check-label" for="vegetarisch"> Vegetarisch</label>
                 </div>
 
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="pizzatoppings" id="vlees">
+                    <input class="form-check-input" type="radio" name="pizzatopping" value="Vlees" id="vlees">
                     <label class="form-check-label" for="vlees"> Vlees</label>
                 </div>
             </div>
@@ -64,22 +85,22 @@
                 <div class="form-label">Kruiden</div>
 
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="peterselie" id="peterselie">
+                    <input class="form-check-input" type="checkbox" name="kruiden[]" value="Peterselie" id="peterselie">
                     <label class="form-check-label" for="peterselie"> Peterselie</label>
                 </div>
 
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="oregano" id="oregano">
+                    <input class="form-check-input" type="checkbox" name="kruiden[]" value="Oregano" id="oregano">
                     <label class="form-check-label" for="oregano"> Oregano</label>
                 </div>
 
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="chili-flakes" id="chili-flakes">
+                    <input class="form-check-input" type="checkbox" name="kruiden[]" value="Chili flakes" id="chili-flakes">
                     <label class="form-check-label" for="chili-flakes"> Chili flakes</label>
                 </div>
 
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="zwarte-peper" id="zwarte-peper">
+                    <input class="form-check-input" type="checkbox" name="kruiden[]" value="Zwarte peper" id="zwarte-peper">
                     <label class="form-check-label" for="zwarte-peper"> Zwarte peper</label>
                 </div>
             </div>
