a6b74b06ba8f6e9107f17855d3280a617c91f064
MbouRowin <117640285+MbouRowin@users.noreply.github.com>
2023-02-03 11:23:35 +0100

    Add more code

---
+++ create.php
@@ -0,0 +1,96 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Maak je eigen pizza</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center">Maak je eigen pizza</h1>
+
+        <form method="post">
+            <div class="mb-3">
+                <label for="bodemformaat" class="form-label">Bodemformaat</label>
+
+                <select name="bodemformaat" id="bodemformaat" class="form-control">
+                    <option>Maak je keuze</option>
+                    <option value="20">20 cm</option>
+                    <option value="25">25 cm</option>
+                    <option value="30">30 cm</option>
+                    <option value="35">35 cm</option>
+                    <option value="40">40 cm</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="bodemformaat" class="form-label">Saus</label>
+
+                <select name="bodemformaat" id="bodemformaat" class="form-control">
+                    <option>Maak je keuze</option>
+                    <option value="tomatensaus">Tomatensaus</option>
+                    <option value="extra-tomatensaus">Extra tomatensaus</option>
+                    <option value="spicy-tomatensaus">Spicy tomatensaus</option>
+                    <option value="bbq-saus">BBQ saus</option>
+                    <option value="creme-fraiche">Creme fraiche</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <div class="form-label">Pizzatoppings</div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="pizzatoppings" id="vegan">
+                    <label class="form-check-label" for="vegan"> Vegan</label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="pizzatoppings" id="vegetarisch">
+                    <label class="form-check-label" for="vegetarisch"> Vegetarisch</label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="pizzatoppings" id="vlees">
+                    <label class="form-check-label" for="vlees"> Vlees</label>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <div class="form-label">Kruiden</div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="peterselie" id="peterselie">
+                    <label class="form-check-label" for="peterselie"> Peterselie</label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="oregano" id="oregano">
+                    <label class="form-check-label" for="oregano"> Oregano</label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="chili-flakes" id="chili-flakes">
+                    <label class="form-check-label" for="chili-flakes"> Chili flakes</label>
+                </div>
+
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="zwarte-peper" id="zwarte-peper">
+                    <label class="form-check-label" for="zwarte-peper"> Zwarte peper</label>
+                </div>
+            </div>
+
+            <div class="d-grid">
+                <button class="btn btn-primary">Bestel</button>
+            </div>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
