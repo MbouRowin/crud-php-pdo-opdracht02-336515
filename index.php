98bf83e6fc9bd7a68677370bb8fb74b7ca3a31a9
MbouRowin <117640285+MbouRowin@users.noreply.github.com>
2023-02-03 10:52:17 +0100

    Add HTML

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Maak je eigen pizza</title>
+</head>
+
+<body>
+    <h1>Maak je eigen pizza</h1>
+</body>
+
+</html>
